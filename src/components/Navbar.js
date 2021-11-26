b26c34527d590f86d494236d9136bd7397bb9961
Xingyu Qiu <xingyuq2@illinois.edu>
2021-11-26 10:01:18 -0600

    edit the style of code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,7 +35,7 @@ function Navbar({user})
                 <div className='navbar-container'>
                     <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                         LOL Wiki
-                        <i class='fas fa-ghost' />
+                        <i className='fas fa-ghost' />
                     </Link>
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
